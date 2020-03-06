ebb6eee7eaca9d87fe3bc349f9ab7d3066cb909d
Plucky Porcupine <pluckyporcupinegb@gmail.com>
2020-03-06 14:01:19 -0500

    clean up snake example

--- games/snake.m
+++ games/snake.m
@@ -82,23 +82,19 @@ proc UpdateGame=
 
         if not pause then
             if IsKeyPressed(KEY_RIGHT) and snek[0].speed.x=0 and allowMove then
-                Vector2 snekSpd:=(SQUARE_SIZE,0)
-                snek[0].speed:=snekSpd
+                snek[0].speed:=(SQUARE_SIZE,0)
                 allowMove:=0
             fi
             if IsKeyPressed(KEY_LEFT) and snek[0].speed.x=0 and allowMove then
-                Vector2 snekSpd2:=(-SQUARE_SIZE,0)
-                snek[0].speed:=snekSpd2
+                snek[0].speed:=(-SQUARE_SIZE,0)
                 allowMove:=0
             fi
             if IsKeyPressed(KEY_UP) and snek[0].speed.y=0 and allowMove then
-                Vector2 snekSpd3:=(0, -SQUARE_SIZE)
-                snek[0].speed:=snekSpd3
+                snek[0].speed:=(0, -SQUARE_SIZE)
                 allowMove:=0
             fi
             if IsKeyPressed(KEY_DOWN) and snek[0].speed.y=0 and allowMove then
-                Vector2 snekSpd4:=(0, SQUARE_SIZE)
-                snek[0].speed:=snekSpd4
+                snek[0].speed:=(0, SQUARE_SIZE)
                 allowMove:=0
             fi
 
@@ -114,15 +110,15 @@ proc UpdateGame=
                 od
             fi
 
-            if snek[0].position.x>screenWidth-offset.x or \
-               snek[0].position.y>screenHeight-offset.y or \
-               snek[0].position.x<0 or \
+            if snek[0].position.x>screenWidth-offset.x or
+               snek[0].position.y>screenHeight-offset.y or
+               snek[0].position.x<0 or
                snek[0].position.y<0 then
                 gameOver:=1
             fi
 
             for i:=1 to counterTail-1 do
-                if snek[0].position.x=snek[i].position.x and \
+                if snek[0].position.x=snek[i].position.x and
                    snek[0].position.y=snek[i].position.y then
                     gameOver:=1
                 fi
@@ -142,9 +138,9 @@ proc UpdateGame=
                 od
             fi
 
-            if snek[0].position.x<fruit.position.x+fruit.size.x and \
-               snek[0].position.x+snek[0].size.x>fruit.position.x and \
-               snek[0].position.y<fruit.position.y+fruit.size.y and \
+            if snek[0].position.x<fruit.position.x+fruit.size.x and
+               snek[0].position.x+snek[0].size.x>fruit.position.x and
+               snek[0].position.y<fruit.position.y+fruit.size.y and
                snek[0].position.y+snek[0].size.y>fruit.position.y then
                 snek[counterTail].position:=snekPos[counterTail-1]
                 counterTail+:=1
@@ -170,17 +166,13 @@ proc DrawGame=
 
         if not gameOver then
             for i:=0 to screenWidth/SQUARE_SIZE do
-                dL1.x:=SQUARE_SIZE*i+offset.x/2
-                dL1.y:=offset.y/2
-                dL2.x:=SQUARE_SIZE*i+offset.x/2
-                dL2.y:=screenHeight-offset.y/2
+                dL1:=(SQUARE_SIZE*i+offset.x/2, offset.y/2)
+                dL2:=(SQUARE_SIZE*i+offset.x/2, screenHeight-offset.y/2)
                 DrawLineV(dL1, dL2, LIGHTGRAY)
             od
             for i:=0 to screenHeight/SQUARE_SIZE do
-                dL1.x:=offset.x/2
-                dL1.y:=SQUARE_SIZE*i+offset.y/2
-                dL2.x:=screenWidth-offset.x/2
-                dL2.y:=SQUARE_SIZE*i+offset.y/2
+                dL1:=(offset.x/2, SQUARE_SIZE*i+offset.y/2)
+                dL2:=(screenWidth-offset.x/2, SQUARE_SIZE*i+offset.y/2)
                 DrawLineV(dL1, dL2, LIGHTGRAY)
             od
 
